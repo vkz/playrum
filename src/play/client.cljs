31e86480ea32f94a42ea5e97f69c78b26912d18c
VLAD KOZIN <ze@zedroit.local>
2017-01-31 15:24:51 -0500

    Add reaction to static-timer component
    Reaction is "top-down" aka quiescent style. Compentent itself is static in the
    sence that it receives immutable arguments and rendering is forced from the
    outside whenever relevant state changes. State change is triggered by a
    timer (tick).
    
    Also started using org-style outlines to group code. Looks clean especially when
    irrelevant code snippets can be collapsed. Some org fontlocking wouldn't hurt
    but even this makes the source file easier to navigate and read.

--- src/play/client.cljs
+++ src/play/client.cljs
@@ -5,20 +5,51 @@
 
 (print "Go out and play!")
 
+;;* State
 (def *clock (atom 0))
 (def *color (atom "#FA8D97"))
+(def *speed (atom 500))
+
+;;* Utils
+(defn- dom-el [^String id]
+  (.getElementById js/document "main"))
 
 (defn format-time [ts]
   (-> ts (js/Date.) (.toISOString) (subs 11 23)))
 
-(rum/defc timer-static < rum/static [label ts]
-  [:div label ": "
-   [:span {:style {:color @*color}} (format-time ts)]])
+(defn tick []
+  (reset! *clock (.getTime (js/Date.)))
+  (js/setTimeout tick @*speed))
 
+;;* Components
+;;** timer-static
+(rum/defc timer-static < rum/static [label time]
+  [:div label ": "
+   [:span {:style {:color @*color}} (format-time time)]])
+
+(defn- mount-timer-static [mount-el]
+  (rum/mount (timer-static "Static" @*clock) mount-el)
+  ;; Setting up watch manually,
+  ;; force top-down re-render via mount
+  (add-watch *clock :timer-static
+             (fn [_ _ _ new-val]
+               (rum/mount (timer-static "Static" new-val) mount-el))))
+
+;;** window
 (rum/defc window < rum/static []
   [:.example
    [:.example-title "Timers"]
    [:#timer-static (timer-static "Static" @*clock)]])
 
 (defn mount []
-  (rum/mount (window) (.getElementById js/document "main")))
+  (rum/mount (window) (dom-el "main")))
+
+;;* Execute
+;;** Mount main window
+(mount)
+
+;;** Mount components
+(mount-timer-static (dom-el "timer-static"))
+
+;;** Start clock
+(tick)
